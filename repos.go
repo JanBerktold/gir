6fd292e88604fc76a0d99eb0519a0b913581627d
Jan Berktold <jan@berktold.co>
2017-02-24 02:46:13 +0100

    Add repos command

--- repos.go
+++ repos.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var ReposCmd = &cobra.Command{
 	Use:   "repos",
-	Short: "",
+	Short: "Gives an overview of all cached repositories",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		data, err := LoadData()
+		if err != nil {
+			fmt.Printf("Failed to load data with error: %q\n", err.Error())
+			os.Exit(-1)
+		}
+
+		for _, repo := range data.Repositories {
+			fmt.Printf("%s/%s (%d Issues)\n", repo.Owner.Name, repo.Name, len(repo.Issues.Issues))
+		}
 	},
 }
 
