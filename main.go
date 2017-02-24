93aaa579921fc9559267931f2c23657648a4b06d
Jan Berktold <jan@berktold.co>
2017-02-24 01:09:26 +0100

    add update logic

--- main.go
+++ main.go
@@ -3,7 +3,38 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
+
+type Repo struct {
+	Owner string
+	Name  string
+}
+
+type Data struct {
+	CurrentRepo  string
+	Repositories []Repo
+}
+
+func LoadRepo(owner, name string) (Repo, error) {
+	return Repo{}, nil
+}
+
+func LoadData() (Data, error) {
+	return Data{}, nil
+}
+
+func SaveData(data Data) error {
+	return nil
+}
+
+func ParseRepo(repo string) (owner, name string) {
+	index := strings.Index(repo, "/")
+	if index > -1 {
+		return repo[0:index], repo[index+1:]
+	}
+	return "", ""
+}
 
 func main() {
 	if err := RootCmd.Execute(); err != nil {
--- update.go
+++ update.go
@@ -1,15 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var UpdateCmd = &cobra.Command{
 	Use:   "update [<owner/name>]",
-	Short: "Updates cached data for specified repository.
-If not repository is given, all data is updated.
-	",
+	Short: "Updates cached data for specified repository.\nIf not repository is given, all data is updated.",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		// parse whether the user specified a target repo
+		var targetOwner, targetName string
+		if len(args) > 0 {
+			targetOwner, targetName = ParseRepo(args[0])
+			if len(targetOwner) == 0 {
+				fmt.Printf("Invalid repo name %q\n", args[0])
+				os.Exit(-1)
+			}
+		}
+
+		data, err := LoadData()
+		if err != nil {
+			fmt.Printf("Failed to load data with error: %q\n", err.Error())
+			os.Exit(-1)
+		}
+
+		if len(targetOwner) == 0 {
+			for i, repo := range data.Repositories {
+				newRepo, err := LoadRepo(repo.Owner, repo.Name)
+				if err != nil {
+					fmt.Printf("Failed to load repo %s/%s: %q\n", repo.Owner, repo.Name, err.Error())
+				} else {
+					data.Repositories[i] = newRepo
+				}
+			}
+		} else {
+			for i, repo := range data.Repositories {
+				if repo.Owner == targetOwner && repo.Name == targetName {
+					newRepo, err := LoadRepo(repo.Owner, repo.Name)
+					if err != nil {
+						fmt.Printf("Failed to load repo %s/%s: %q\n", repo.Owner, repo.Name, err.Error())
+						os.Exit(-1)
+					}
+					data.Repositories[i] = newRepo
+					goto finished
+				}
+			}
+		}
+
+	finished:
+		if err := SaveData(data); err != nil {
+			fmt.Printf("Failed to write to disk: %q\n", err.Error())
+			os.Exit(-1)
+		}
 	},
 }
 
